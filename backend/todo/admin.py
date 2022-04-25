b7b195360d922ca83e56463ee3db0ff31286dde4
Tanvir Rahman <trahman@paradynamix.com>
2022-04-25 23:29:31 +0600

    api backend initiate

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+# Register the model here,
+admin.site.register(Todo, ToDoAdmin)
